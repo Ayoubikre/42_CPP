24bd01b89799560c441865fcf52b2eea19738824
Ikr <ayoubikrr@gmail.com>
2026-09-16 21:54:13 +0100

    cpp_07 60%

--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -1,19 +1,12 @@
 #include "iter.hpp"
 
-#include <stdlib.h>
-
 void ft_p(int a)
 {
     ft_log("print -> " << a);
 }
-void aa()
-{
-system("leaks Ex");
-}
+
 int main( void ) {
-    
-    atexit(aa);
-    int *c=new int[3];
+        int *c=new int[3];
 
     for(int i=0;i<3;i++)
         c[i]=i+1;
